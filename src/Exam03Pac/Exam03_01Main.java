721d7253a1fc38f42d7ee8c3c64cdb8b2488e239
DrInnovatio <turing1723@gmail.com>
2021-12-18 17:58:59 +0900

    Method : compareTo(Object)

---
+++ src/Exam03Pac/Exam03_01Main.java
@@ -0,0 +1,13 @@
+package Exam03Pac;
+
+public class Exam03_01Main {
+
+    public static void main(String[] args) {
+        Integer a = new Integer(3);
+        Integer b = new Integer(5);
+
+        System.out.println("a, b  = " + a.compareTo(b));
+    }
+}
+
+// Method : compareTo(Object)
