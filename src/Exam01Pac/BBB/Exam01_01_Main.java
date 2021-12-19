cda779cd29816d646fbaffe96e05218e893846e9
DrInnovatio <turing1723@gmail.com>
2021-12-19 22:01:11 +0900

    start.

---
+++ src/Exam01Pac/BBB/Exam01_01_Main.java
@@ -0,0 +1,10 @@
+package Exam01Pac.BBB;
+
+public class Exam01_01_Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/Exam02Pac/AAA/Exam02_01.java
@@ -0,0 +1,19 @@
+package Exam02Pac.AAA;
+
+public class Exam02_01 extends Object {
+
+    private String str;
+
+    public Exam02_01(){
+        this.str = "Hello there!";
+    }
+
+    public Exam02_01(String str){
+        this.str = str;
+    }
+
+     public String toString(){
+         return str;
+     }
+
+}
